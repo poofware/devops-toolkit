efadc98a824231f23e92e4f38061897b72812efa
Jacob Moore <jlmoors001@gmail.com>
2025-04-11 15:20:08 -0500

    update: moved ensure go into running only if vendoring is enabled

--- backend/make/utils/vendor.mk
+++ backend/make/utils/vendor.mk
@@ -14,7 +14,6 @@ endif
 
 ## Updates vendor directory based on changes in go.mod or go.sum (disabled if vendor directory is empty, enable by running 'go mod vendor' once)
 vendor.stamp: go.mod go.sum
-	@$(MAKE) --no-print-directory _ensure-go
 	@if [ ! -d "vendor" ]; then \
 		echo "[INFO] [Vendor] Initializing empty vendor directory..."; \
 		mkdir -p vendor; \
@@ -22,6 +21,7 @@ vendor.stamp: go.mod go.sum
 	@if [ -z "$$(ls -A vendor)" ]; then \
 		echo "[WARN] [Vendor] Vendoring is disabled due to an empty vendor directory. Enable by running 'go mod vendor' once."; \
 	else \
+		@$(MAKE) --no-print-directory _ensure-go
 		echo "[INFO] [Vendor] Vendoring is enabled due to the presence of a non-empty vendor directory."; \
 		echo "[INFO] [Vendor] Updating vendor due to changes in go.mod or go.sum..."; \
 		go mod vendor; \
